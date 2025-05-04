80bbf0898269df827c80267996ac7078b267f678
unknown <ernestoaleman00@gmail.com>
2025-05-04 00:57:47 -0500

    Init SavedCandidate & prebuilt styles. Awaiting return statement

--- src/components/SavedCandidate.tsx
+++ src/components/SavedCandidate.tsx
@@ -7,6 +7,41 @@ type SavedCandidateProps = {
   rejectCandidate: (id: number) => void;
 };
 
+const SavedCandidate = ({ candidate, rejectCandidate }: SavedCandidateProps) => {
+  if (!candidate) {
+    return (
+      <tr>
+        <td colSpan={7}>
+          <h2>No Candidates at this time</h2>
+        </td>
+      </tr>
+    );
+  }
+  
+  // Extracted styles
+  const styles = {
+    avatar: {
+      width: '70px',
+      borderRadius: '10px',
+      display: 'block',
+      margin: '0 auto',
+    },
+    name: {
+      color: 'white',
+    },
+    removeButton: {
+      color: 'red',
+      margin: '0 auto',
+      display: 'block',
+      cursor: 'pointer',
+      fontSize: '50px',
+    },
+  };
+
+
+
+  
+}
 
 
 
