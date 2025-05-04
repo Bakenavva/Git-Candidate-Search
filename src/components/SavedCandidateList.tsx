a841631c7abce25a0867e7d5671186e4a036606e
unknown <ernestoaleman00@gmail.com>
2025-05-04 01:01:44 -0500

    For SavedCandidateList, implemented useEffect

--- src/components/SavedCandidateList.tsx
+++ src/components/SavedCandidateList.tsx
@@ -3,9 +3,16 @@ import SavedCandidate from './SavedCandidate';
 import type { Candidate } from '../interfaces/Candidate.interface';
 
 const SavedCandidateList = () => {
-    const [potentialCandidates, setPotentialCandidates] = useState<Candidate[]>([]);
+  const [potentialCandidates, setPotentialCandidates] = useState<Candidate[]>([]);
 
-
+  useEffect(() => {
+    const savedCandidates = localStorage.getItem('savedCandidates');
+    const candidates = savedCandidates ? JSON.parse(savedCandidates) : [];
+    const validCandidates = candidates.filter(
+      (candidate: Candidate) => candidate.id && candidate.login
+    );
+    setPotentialCandidates(validCandidates);
+  }, []);
     
 
 
