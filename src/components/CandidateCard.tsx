ce0ea7aba01d9f668f226ff61b37e11c711b340b
unknown <ernestoaleman00@gmail.com>
2025-05-04 00:54:01 -0500

    Setting up CandidateCard. Awaiting returned "html"

--- src/components/CandidateCard.tsx
+++ src/components/CandidateCard.tsx
@@ -5,3 +5,34 @@ type CandidateCardProps = {
   currentUser: Candidate;
   userChoice: (isSelected: boolean) => void;
 };
+
+const CandidateCard = ({ currentUser, userChoice }: CandidateCardProps) => {
+  if (!currentUser) {
+    return <h2>No Candidates at this time</h2>;
+  }
+
+  const {
+    avatar_url, 
+    html_url,
+    login,
+    name,
+    location,
+    email,
+    company,
+    bio,
+  } = currentUser;
+    
+  const avatarStyle = { width: '300px', borderRadius: '30px 30px 0 0' };
+  const cardStyle = { backgroundColor: '#000', width: '280px', borderRadius: '0 0 30px 30px', padding: '0 10px 10px' };
+  const buttonStyle = { fontSize: '80px', cursor: 'pointer' };
+    
+  return (
+
+
+
+
+  )
+
+}  
+
+export default CandidateCard;
