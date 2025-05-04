3d68c6b18ae2e46e7782f72828fbcb58e07d9545
unknown <ernestoaleman00@gmail.com>
2025-05-04 01:00:47 -0500

    Set up SavedCandidateList component with imports, export, and const

---
+++ src/components/SavedCandidateList.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import SavedCandidate from './SavedCandidate';
+import type { Candidate } from '../interfaces/Candidate.interface';
+
+const SavedCandidateList = () => {
+    const [potentialCandidates, setPotentialCandidates] = useState<Candidate[]>([]);
+
+
+    
+
+
+
+}
+
+export default SavedCandidateList;
