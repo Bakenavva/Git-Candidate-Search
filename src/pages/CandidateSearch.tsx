658e113d341d58bbe649dbcb7c5a4ae430207872
unknown <ernestoaleman00@gmail.com>
2025-05-04 01:09:28 -0500

    Set initial config to CandidateSearch.tsx page for maintenance

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,26 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import type {Candidate} from '../interfaces/Candidate.interface';
+import CandidateCard from '../components/CandidateCard';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
-};
+  const [results, setResults] = useState<Candidate[]>([]);
+  const [currentUser, setCurrentUser] = useState<Candidate | null>(null);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  // Fetch initial list of candidates on component mount
+  useEffect(() => {
+    fetchCandidates();
+  }, []);
+
+
+
+  return (
+
+
+
+  )
+
+}
 
 export default CandidateSearch;
